aece0f714daa145a17c732a180cd8c8699b15b2b
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2022-09-07 13:16:29 +0200

    ghvj

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+Tamagotchi t = new Tamagotchi();
+t.name = "Henry";
+t.Tick();
+t.Tick();
+t.Tick();
+t.PrintStats();
+t.Feed();
+t.PrintStats();
+Console.WriteLine(t.GetAlive());
+
+Console.ReadLine();
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -4,11 +4,55 @@ public class Tamagotchi
 {
     int hunger;
     int boredom;
-    bool isAlive;
+    bool isAlive = true;
+    Random generator = new Random();
     public string name;
 
     public void Feed()
     {
+        Console.WriteLine($"{name} eats and is less hungry.");
+        hunger -= 1;
 
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public void Teach(string word)
+    {
+
+    }
+
+    public void Tick()
+    {
+        hunger++;
+        boredom++;
+
+        if(hunger > 10){
+            isAlive = false;
+        }
+
+        if(boredom > 10){
+            isAlive = false;
+        }  
+
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"{name}: Hunger: {hunger} Boredom: {boredom} ");
+
+    }
+
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
+
+    void ReduceBoredom()
+    {
+        boredom -= 1;
     }
 }
