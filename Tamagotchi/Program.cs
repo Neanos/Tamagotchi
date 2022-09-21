a5332564a75dfb934496c46f216fd7994a0c0513
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2022-09-21 13:11:16 +0200

    guö

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -9,12 +9,14 @@ Console.WriteLine($"{t1.name}");
 while(t1.GetAlive() == true)
 {
     Console.Clear();
-    //t1.PrintStats();
+    t1.PrintStats();
     Console.WriteLine($"What would you like to do with {t1.name} today?");
     Console.WriteLine($"1) Feed {t1.name}");
     Console.WriteLine($"2) Teach {t1.name} a new word");
     Console.WriteLine($"3) Talk to {t1.name}");
-    Console.WriteLine("4) Do nothing");
+    Console.WriteLine($"4) Throw ball with {t1.name}");
+    Console.WriteLine($"5) Play with {t1.name} and a dog");
+    Console.WriteLine("6) Do nothing");
 
     string action = Console.ReadLine();
     if(action == "1"){
@@ -32,7 +34,19 @@ while(t1.GetAlive() == true)
         Console.ReadLine();
     }
 
-    if(action == "4"){
+    if(action == "4")
+    {
+        t1.Ball();
+        Console.ReadLine();
+    }
+
+    if(action =="5")
+    {
+        t1.Dog();
+        //Console.ReadLine();
+    }
+
+    if(action == "6"){
         Console.WriteLine("Doing nothing");
     }
 
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -8,6 +8,9 @@ public class Tamagotchi
     bool isAlive = true;
     private List<string> words = new List<string>(){"Hi"};
     Random generator = new Random();
+    Random generator2 = new Random();
+    Random generator3 = new Random();
+    int attraper;
     public string name = "Alice";
 
     public void Feed()
@@ -61,10 +64,48 @@ public class Tamagotchi
         return isAlive;
     }
 
+    public void Ball()
+    {
+        int attraper = generator2.Next(2);
+        if(attraper == 0)
+        {
+            Console.WriteLine($"{name} catches the ball!");
+        }
+
+        if(attraper == 1)
+        {
+            Console.WriteLine($"{name} misses the ball");
+        }
+
+        ReduceBoredom();
+
+    }
+
+    public void Dog()
+    {
+        int dog = generator3.Next(2);
+
+        if(dog == 0)
+        {
+            Console.WriteLine($"Oh no! {name} didn't out run the dog!");
+            isAlive = false;
+        }
+        else
+        {
+            Console.WriteLine($"{name} managed to out run the dog and got away. This time.");
+            isAlive = true;
+            ReduceBoredom();
+        }
+
+    }
+
     void ReduceBoredom()
     {
         boredom -= 2;
         Console.WriteLine($"{name} is now less bored!");
         Console.ReadLine();
     }
+
+
 }
+
